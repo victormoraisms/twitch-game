5a37b98c38e375e7282ba9ef8dc8acc502ad47e2
victormoraisms-transformco <victormoraisms@gmail.com>
2026-01-08 13:03:27 -0300

    refactor: container-lg set to 70rem

--- twitch-game/components/streams-modal.tsx
+++ twitch-game/components/streams-modal.tsx
@@ -68,7 +68,10 @@ export function StreamsModal({ isOpen, onClose, gameId, gameName }: StreamsModal
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="w-[90vw] max-w-6xl h-[80vh] max-h-[80vh] overflow-y-auto">
+      <DialogContent 
+        className="w-[90vw] max-w-6xl h-[80vh] max-h-[80vh] overflow-y-auto"
+        style={{ "--container-lg": "70rem" } as React.CSSProperties}
+      >
         <DialogHeader>
           <DialogTitle className="text-2xl md:text-4xl font-bold">
             Top Streams: {gameName}
