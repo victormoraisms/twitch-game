340cb9a6e93ea90eab0536d57fc82b10fd58ff92
victormoraisms-transformco <victormoraisms@gmail.com>
2026-01-08 14:36:30 -0300

    feat: Adding leaderboard board.

--- twitch-game/app/page.tsx
+++ twitch-game/app/page.tsx
@@ -1,5 +1,6 @@
 import { getRandomGamePair } from "@/app/actions/getGames"
 import { GameClient } from "@/components/game-client"
+import { Leaderboard } from "@/components/leaderboard"
 
 export const revalidate = 60
 
@@ -7,5 +8,14 @@ export default async function HigherOrLowerGame() {
   // Fetch initial game pair from Twitch API on the server
   const initialGamePair = await getRandomGamePair()
 
-  return <GameClient initialGamePair={initialGamePair} />
+  return (
+    <div className="flex flex-col lg:flex-row gap-6 lg:gap-8">
+      <div className="flex-1">
+        <GameClient initialGamePair={initialGamePair} />
+      </div>
+      <aside className="w-full lg:w-80 flex-shrink-0">
+        <Leaderboard />
+      </aside>
+    </div>
+  )
 }
